e581347668234c9a080b355d20de7a2bfb795407
Heyo <csxc.me@gmail.com>
2018-05-03 20:00:48 +0800

    star

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,20 +13,61 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div class="support-count" v-if="seller.supports" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div class="detail" v-show="detailShow">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <star :score="seller.score" :size="48"></star>
+          </div>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import star from '../star/star'
 export default {
+  components: {
+    star
+  },
   props: {
     seller: {
       type: Object
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  data() {
+    return {
+      detailShow: false
+    }
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true
     }
   }
 }
@@ -36,40 +77,144 @@ export default {
 
 .header
   color: #fff
-  background: #000
+  position: relative
+  overflow: hidden
+  background: rgba(7, 17, 27, 0.5)
   .content-wrapper
+    position: relative
     padding: 24px 12px 18px 24px
     font-size: 0
     .avatar
       display: inline-block
-      vertical-align top
-      img 
-        border-radius 2px
+      vertical-align: top
+      img
+        border-radius: 2px
     .content
       display: inline-block
       margin-left: 14px
       font-size: 14px
-    .title
-      margin: 2px 0 8px 0
-      .brand
-        display: inline-block
+      .title
+        margin: 2px 0 8px 0
+        .brand
+          display: inline-block
+          vertical-align: top
+          width: 30px
+          height: 18px
+          bg-image('brand')
+          background-size: 30px 18px
+          background-repeat: no-repeat
+        .name
+          margin-left: 6px
+          font-size: 16px
+          line-height: 18px
+          font-weight: bold
+      .description
+        margin-bottom: 10px
+        line-height: 12px
+        font-size: 12px
+      .support
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 6px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_1')
+          &.discount
+            bg-image('discount_1')
+          &.guarantee
+            bg-image('guarantee_1')
+          &.invoice
+            bg-image('invoice_1')
+          &.special
+            bg-image('special_1')
+        .text
+          line-height: 12px
+          font-size: 12px
+    .support-count
+      position: absolute
+      right: 12px
+      bottom: 14px
+      padding: 0 8px
+      height: 24px
+      line-height: 24px
+      border-radius: 14px
+      background: rgba(0, 0, 0, 0.2)
+      text-align: center
+      .count
         vertical-align: top
-        width: 30px
-        height: 18px
-        bg-image('brand')
-        background-size: 30px 18px
-        background-repeat: no-repeat
-      .name
-        margin-left: 6px
-        font-size: 16px
-        line-height: 18px
-        font-weight: bold
-    .description
-      margin-bottom 10px
-      line-height 12px
-      font-size 12px 
-    .support
-      margin-bottom 10px
-      line-height 12px
-      font-size 12px 
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        margin-left: 2px
+        line-height: 24px
+        font-size: 10px
+  .bulletin-wrapper
+    position: relative
+    height: 28px
+    line-height: 28px
+    padding: 0 22px 0 12px
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
+    background: rgba(7, 17, 27, 0.1)
+    .bulletin-title
+      display: inline-block
+      vertical-align: top
+      margin-top: 8px
+      width: 22px
+      height: 12px
+      bg-image('bulletin')
+      background-size: 22px 12px
+      background-repeat: no-repeat
+    .bulletin-text
+      vertical-align: top
+      margin: 0 4px
+      font-size: 10px
+    .icon-keyboard_arrow_right
+      position: absolute
+      font-size: 10px
+      right: 12px
+      top: 8px
+  .background
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    z-index: -1
+    filter: blur(10px)
+  .detail
+    position: fixed
+    z-index: 100
+    top: 0
+    left: 0
+    height: 100%
+    width: 100%
+    overflow: auto
+    background: rgba(7, 17, 27, 0.8)
+    .detail-wrapper
+      min-height: 100%
+      width: 100%
+      .detail-main
+        margin-top: 64px
+        padding-bottom: 64px
+        .name
+          line-height: 16px
+          text-align: center
+          font-size: 16px
+          font-weight: 700
+        .star-wrapper
+          margin-top 18px
+          padding 2px 0
+          text-align center
+    .detail-close
+      position: relative
+      width: 32px
+      height: 32px
+      margin: -64px auto 0 auto
+      clear: both
+      font-size: 32px
 </style>
