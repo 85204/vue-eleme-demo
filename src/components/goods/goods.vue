e96f849b38d16a7ca04c95be8ae559fc8dc08e1d
Heyo <csxc.me@gmail.com>
2018-05-04 23:21:21 +0800

    引入better scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li class="menu-item" v-for="item in goods" :key="item.name">
           <span class="text border-1px">
@@ -9,12 +9,36 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul>
+        <li class="foods-list" v-for="item in goods" :key="item.name">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li class="food-item border-1px" v-for="food in item.foods" :key="food.name">
+              <div class="icon">
+                <img width="57px" :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import icon from '../icon/icon'
+import BScroll from 'better-scroll'
 
 const ERR_OK = 0
 
@@ -38,8 +62,17 @@ export default {
       const data = res.data
       if (data.errno === ERR_OK) {
         this.goods = data.data
+        this.$nextTick(() => {
+          this._initScroll()
+        })
       }
     })()
+  },
+  methods: {
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {})
+    }
   }
 }
 </script>
@@ -76,4 +109,51 @@ export default {
           margin-top: 1px
   .foods-wrapper
     flex: 1
+    .title
+      padding-left: 14px
+      height: 26px
+      line-height: 26px
+      border-left: 2px solid #d9dde1
+      font-size: 12px
+      color: rgb(147, 153, 159)
+      background: #f3f5f7
+    .food-item
+      display: flex
+      margin: 18px
+      padding-bottom: 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        margin-bottom: 0
+        border-none()
+      .icon
+        flex: 0 0 57px
+        margin-right: 10px
+      .content
+        flex: 1
+        .name
+          margin: 2px 0 8px 0
+          height: 14px
+          line-height: 14px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .desc, .extra
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .desc
+          line-height: 12px
+          margin-bottom: 8px
+        .extra
+          .count
+            margin-right: 12px
+        .price
+          font-weight: 700
+          line-height: 24px
+          .now
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240, 20, 20)
+          .old
+            font-size: 10px
+            text-decoration: line-through
 </style>
